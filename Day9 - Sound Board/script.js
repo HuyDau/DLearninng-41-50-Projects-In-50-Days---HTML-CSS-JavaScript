afb5dec9381d18fc6401620d954a5062c618b109
HuyDau <65066673+HuyDau@users.noreply.github.com>
2024-02-27 18:03:21 +0700

    27-2-2024

---
+++ Day9 - Sound Board/script.js
@@ -0,0 +1,28 @@
+const sounds = ['sound1', 'sound2', 'sound3', 'sound4']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0
+
+    })
+}
+
+
