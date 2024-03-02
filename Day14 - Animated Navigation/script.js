2a4ef5e216b683a56f917fb515a7e37c32db3cae
HuyDau <65066673+HuyDau@users.noreply.github.com>
2024-03-02 11:12:17 +0700

    322024

---
+++ Day14 - Animated Navigation/script.js
@@ -0,0 +1,10 @@
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+
+// toggle.addEventListener('click', ()=>{
+//     nav.classList.add("active")
+// })
+
+function active(){
+    nav.classList.toggle("active")
+}
